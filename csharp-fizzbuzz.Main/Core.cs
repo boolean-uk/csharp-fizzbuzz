5700ff0ad746b2360e33dce069a04436ce96b561
Nikita Pieters <nikita_pieters@hotmail.com>
2023-05-01 18:03:58 +0200

    made a start at core Fizzbuzz

--- csharp-fizzbuzz.Main/Core.cs
+++ csharp-fizzbuzz.Main/Core.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,17 +20,42 @@ namespace csharp_fizzbuzz.Main
 
         public String[] fizzbuzz(String[] nums)
         {
-            throw new NotImplementedException();
-
-            // TODO: 1. Implement a function that will look through a list of String representation of numbers and
+           // TODO: 1. Implement a function that will look through a list of String representation of numbers and
             /* 
                 change the values of said array based on the rules below
                 Where a number is a multiple of three (3, 6, 9, etc) the number in the array should be replaced the string "Fizz.
                 Where a number is a multiple of five (5, 10, etc) the number in the array should be replaced the string "Buzz".
                 Where a number is a multiple of both five AND three (15, 30, etc) the number in the array should be replaced the string "Fizzbuzz".
             * */
-
-
+            //foreach (String s in nums)
+            //{
+            //    try { 
+            //        int number = Int32.Parse(s); 
+            //    }
+            //    catch { }
+                int[] numberArray = new int[nums.Length];
+                for (int i = 0; i<= nums.Length; i++)
+                {
+                    numberArray[i] = int.Parse(nums[i]);
+                }
+            for (int i = 0; i <= numberArray.Length; i++)
+            {
+                if (numberArray[i] % 3 == 0)
+                {
+                    nums[i].Replace(nums[i], "Fizz");
+                        //replace with Fizz in nums
+                }
+                else if (numberArray[i] % 5 == 0)
+                {
+                    nums[i].Replace(nums[i], "Buzz");
+                    //replace with Buzz in nums
+                }
+                else if (numberArray[i] % 3 == 0 && numberArray[i] % 5 == 0)
+                {
+                    nums[i].Replace(nums[i], "Fizzbuzz");
+                    //replace with Fizzbuzz in nums
+                }
+            }
 
             return nums;
         }
