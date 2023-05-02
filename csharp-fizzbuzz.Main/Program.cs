13f910b4430ff7652298d118ac66fc717876b9e7
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-05-02 11:57:02 +0300

    core done

--- csharp-fizzbuzz.Main/Program.cs
+++ csharp-fizzbuzz.Main/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_fizzbuzz.Main;
+
 Console.WriteLine("Hello, World!");
+
+Core core = new Core();
+String[] numsOne = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" };
+Console.WriteLine(core.fizzbuzz(numsOne));
