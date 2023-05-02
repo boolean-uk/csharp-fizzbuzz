6fdc4c64d450e1c60415c1dec1200ab02b2039bd
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-05-02 13:23:20 +0300

    done with extensions

--- csharp-fizzbuzz.Main/Program.cs
+++ csharp-fizzbuzz.Main/Program.cs
@@ -6,3 +6,7 @@ Console.WriteLine("Hello, World!");
 Core core = new Core();
 String[] numsOne = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" };
 Console.WriteLine(core.fizzbuzz(numsOne));
+
+Extension extension = new Extension();
+
+Console.WriteLine(extension.fizzbuzz(1,15));
