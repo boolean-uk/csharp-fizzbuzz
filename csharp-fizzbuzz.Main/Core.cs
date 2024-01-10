56f173de0c2b6cba07e9bb5acebc87ff2ba9559e
Jens Arvid Forgård <jensarvid99@gmail.com>
2024-01-10 11:28:30 +0100

    Jens Arvid Forgård

--- csharp-fizzbuzz.Main/Core.cs
+++ csharp-fizzbuzz.Main/Core.cs
@@ -18,17 +18,27 @@ namespace csharp_fizzbuzz.Main
 
         public String[] fizzbuzz(String[] nums)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                
+                    int num = int.Parse(nums[i]);
 
-            // TODO: 1. Implement a function that will look through a list of String representation of numbers and
-            /* 
-                change the values of said array based on the rules below
-                Where a number is a multiple of three (3, 6, 9, etc) the number in the array should be replaced the string "Fizz.
-                Where a number is a multiple of five (5, 10, etc) the number in the array should be replaced the string "Buzz".
-                Where a number is a multiple of both five AND three (15, 30, etc) the number in the array should be replaced the string "Fizzbuzz".
-            * */
-
-
+                    if (num % 3 == 0 && num % 5 == 0)
+                    {
+                        nums[i] = "Fizzbuzz";
+                    }
+                    else if (num % 3 == 0)
+                    {
+                        nums[i] = "Fizz";
+                    }
+                    else if (num % 5 == 0)
+                    {
+                        nums[i] = "Buzz";
+                    }
+               
+                
+                                  
+            }
 
             return nums;
         }
--- csharp-fizzbuzz.Main/Extension.cs
+++ csharp-fizzbuzz.Main/Extension.cs
@@ -10,18 +10,31 @@ namespace csharp_fizzbuzz.Main
     {
         public string[] fizzbuzz(int lower, int higher)
         {
-            throw new NotImplementedException();
+            List<string> liste = new List<string>();
 
-            //TODO: Extension: Implement a function that will create a list of strings containing the numbers from the lower number to the higher number
-            /* 
-                Where a number is a multiple of three (3, 6, 9, etc) the array should contain the string "Fizz" instead of a number.
-                Where a number is a multiple of five (5, 10, etc) the array should contain the string "Buzz" instead of a number.
-                Where a number is a multiple of both five AND three (15, 30, etc) the array should contain the string "Fizzbuzz" instead of a number.
-            * */
+            for (int i = lower; i <= higher; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    liste.Add("Fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    liste.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    liste.Add("Buzz");
+                }
+                else
+                {
+                    liste.Add(i.ToString());
+                }
+            }
 
-
-
-            return null;
+            return liste.ToArray();
         }
     }
+
 }
+
